f806211e7b68f4b5af575bbb309cc395dd007acc
Paul <paul.du56@gmail.com>
2018-01-26 15:36:55 -0500

    ok

---
+++ insertSpells.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("sorts");
+    var sortsJson = fs.readFileSync("sorts.json");
+    var sorts = JSON.parse(sortsJson);  
+    dbo.collection("sorts").insertMany(sorts, function(err, res) {
+        if (err) throw err;
+        console.log("Number of spell inserted: " + res.insertedCount);
+        db.close();
+  });
+});
