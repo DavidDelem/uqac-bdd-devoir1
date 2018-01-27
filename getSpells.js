aa4f60196be4a6402cd0708425d87afddf102128
renald-morice <renaldmorice@hotmail.fr>
2018-01-26 23:12:38 -0500

    insert all spells

--- getSpells.js
+++ getSpells.js
@@ -19,26 +19,23 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
         var is_verbal = false;
         
         //Check s'il s'agit d'un sort wizard, maximum niveau 4
-        for(i = 0; i < this.levels.length; i++){
-            
-            //'i' insensible à la casse
-            if(this.levels[i].class.search(/wizard/i) != -1 && this.levels[i].level <= 4) is_wizard = true;
+        if(this.levels !== undefined){
+            for(i = 0; i < this.levels.length; i++){
+                //'i' insensible à la casse
+                if(this.levels[i].class.search(/wizard/i) != -1 && this.levels[i].level <= 4) is_wizard = true;
+            }
         }
         
         //Check s'il s'agit d'un sort verbal
-        for(i = 0; i < this.components.length; i++){
-            
-            if(this.components[i] == "V") is_verbal = true;
-        }
+        if(this.components!== undefined && this.components.length == 1 && this.components[0] == "V") is_verbal = true;
         
-        if(is_wizard && is_verbal) emit(this.name, 1);
+        if(is_wizard && is_verbal) emit(this._id, 1);
     };
     
     
     var reduce = function(key, values){
-        return values;
+        return values.toString();
     };
-    
     
     sorts.mapReduce(map, reduce, {out: {inline : 1}}, function(err, result){
         assert.equal(null, err);
