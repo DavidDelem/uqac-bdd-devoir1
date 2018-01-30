21420f58a6b053cfe82cd56f7829518f8633d972
LBotho <loic.bothorel@icloud.com>
2018-01-30 10:25:35 -0500

    Add SQLITE request

--- getSpellsSQlite.js
+++ getSpellsSQlite.js
@@ -9,3 +9,35 @@ var db = new sqlite3.Database('./sqlitedb/sorts.db', (err) => {
   if (err) console.error(err.message);
   console.log('Connected to the sorts database.');
 });
+
+var sql = `
+SELECT
+	s._id,
+    s._name
+FROM
+	SORT s,
+	SORT_LEVEL sl,
+	LEVELS l,
+	SORT_COMPONENT sc,
+	COMPONENT c
+WHERE
+	s._id = sl._id_sort
+	AND s._id = sc._id_sort
+	AND l._id = sl._id_level
+	AND c._id = sc._id_component
+	
+	AND CONTAINS(l.name, 'wizard')
+	AND sl.value <= 4
+	AND c.name = 'V'`;
+ 
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log("Id: "+row._id+", Name: "+row._name);
+  });
+});
+ 
+// close the database connection
+db.close();
