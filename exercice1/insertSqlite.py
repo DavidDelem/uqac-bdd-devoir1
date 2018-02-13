c5c4d7f663bd9efebf97f96defc04b9fb65452e7
Paul <paul.du56@gmail.com>
2018-02-13 13:51:10 -0500

    OK

--- exercice1/insertSqlite.py
+++ exercice1/insertSqlite.py
@@ -77,32 +77,25 @@ def find_comp(myComp, allComp):
         
 def select_sort_de_la_morkitu(conn):
     cur = conn.cursor()
-#    cur.execute("""
-#            SELECT
-#                s._id id,
-#                s.name name
-#            FROM
-#                SORT s,
-#                SORT_LEVEL sl,
-#                LEVEL l,
-#                SORT_COMPONENT sc,
-#                COMPONENT c
-#            WHERE
-#                s._id = sl._id_sort
-#                AND s._id = sc._id_sort
-#                AND l._id = sl._id_level
-#                AND c._id = sc._id_component
-#
-#                AND l.name LIKE '%wizard%'
-#                AND sl._value <= 4
-#                AND c.name = 'V'
-#            """)
-
     cur.execute("""
             SELECT
-                *
+                s._id id,
+                s.name name
             FROM
-                SORT_COMPONENT sc                
+                SORT s,
+                SORT_LEVEL sl,
+                LEVEL l,
+                SORT_COMPONENT sc,
+                COMPONENT c
+            WHERE
+                s._id = sl._id_sort
+                AND s._id = sc._id_sort
+                AND l._id = sl._id_level
+                AND c._id = sc._id_component
+                AND l.name LIKE '%wizard%'
+                AND sl._value <= 4
+                AND c.name = 'V'
+                AND (SELECT count(*) FROM SORT_COMPONENT sc2 WHERE sc2._id_sort = s._id ) = 1;               
             """)
 
     rows = cur.fetchall()
