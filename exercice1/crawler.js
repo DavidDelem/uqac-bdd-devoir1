9b426ccd1289031cea130d7aa8c0eb28538d1090
renald-morice <renaldmorice@hotmail.fr>
2018-02-21 23:53:00 -0500

    SQL & MongoDB JS fix

--- exercice1/crawler.js
+++ exercice1/crawler.js
@@ -4,163 +4,147 @@
 /*-------------------------------------------------------------------------------------*/
 /*-------------------------------------------------------------------------------------*/
 
-
-var express = require('express');
-var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var async = require("async");
-var app = express();
 var insertSpells = require('./insertSpells.js');
 
-/* Lorsque l'utilisateur fera une requête à localhost:8088/sorts, les sorts seront récupérés par Crawling */
 
-app.get('/sorts', function(req, res){
+var baseUrl = 'http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID='
+var sorts = [];
 
-    var baseUrl = 'http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID='
-    var sorts = [];
-    
-    // Parcours de tous les sorts un a un
-    var array = Array.from(Array(/*1975*/500),(x,i)=>i+1);
-    
-    console.log("Début de la récupération des sorts");
-    
-    async.each(array, function(item, callback) {
+// Parcours de tous les sorts un a un
+var array = Array.from(Array(1975),(x,i)=>i+1);
 
-            // Réalisation de la requête
-            request('http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=' + item, function(error, response, html) {
+console.log("Début de la récupération des sorts à l'adresse http://www.dxcontent.com/SDB_SpellBlock.asp ("+ array.length + " sorts à récupérer)");
 
-                if(!error) {
+async.each(array, function(item, callback) {
 
-                    // On utilise la librairie Cheerio sur le code HTML renvoyé, qui nous offre les fonctionnalités d'analyse du DOM de jQuery
-                    // Doc de Cheerios: https://cheerio.js.org
+        // Réalisation de la requête
+        request('http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=' + item, function(error, response, html) {
 
-                    var $ = cheerio.load(html);
+            if(!error) {
 
-                    var jsonSortElem = new Object();
-                    jsonSortElem._id = item;
+                // On utilise la librairie Cheerio sur le code HTML renvoyé, qui nous offre les fonctionnalités d'analyse du DOM de jQuery
+                // Doc de Cheerios: https://cheerio.js.org
 
-                    // Capture des éléments dans le HTML
+                var $ = cheerio.load(html);
 
-                    $('.heading:first-of-type').filter(function() {
-                        var data = $(this);
-                        jsonSortElem.name = data.children().text();
+                var jsonSortElem = new Object();
+                jsonSortElem._id = item;
+
+                // Capture des éléments dans le HTML
+
+                $('.heading:first-of-type').filter(function() {
+                    var data = $(this);
+                    jsonSortElem.name = data.children().text();
+                });
+
+                $('.SPDet').each(function(i, elem) {
+                    var data = $(this).html(); // doit renvoyer un truc du style (exemple): <b>School</b> evocation <b>Level</b> paladin 4, ranger 4
+                    // regarder le contenu de tous les <b>, si un des <b> contient un des éléments qu'on recherche, récupérer le texte qui est juste après
+                    var res = data.split("<b>");
+                    res.forEach(function(element) {
+                        if (element.includes("School")) {
+                            var school = element.split("</b> ")[1];
+                            jsonSortElem.school = school;
+                        }
+                        if (element.includes("Level")) {
+                            var res = element.split("</b> ")[1];
+                            res = element.split(",");
+                            var levels = [];
+                            res.forEach(function(element) {
+                                var res2 = element.split(" ");
+                                if(!levels.some(item => item.class == res2[1])) levels.push({"class":res2[1],"level":res2[2]});
+                            });
+                            jsonSortElem.levels = levels;
+                        }
+                        if (element.includes("Casting Time")) {
+                            var casting_time = element.split("</b> ")[1];
+                            jsonSortElem.casting_time = casting_time;
+                        }
+                        if (element.includes("Components")) {
+
+                            var res = element.split("</b> ")[1];
+
+                            var components = [];
+                            var regex = / *,? *(V|S|F|M|DF|F\/DF|M\/DF) *,? */g;
+                            while (match = regex.exec(res)){
+                                if(components.indexOf(match[1]) == -1) components.push(match[1]);
+                            }
+                            jsonSortElem.components = components;
+
+                            /*var res = element.split("</b> ")[1];
+                            res = element.split(",");
+                            var components = [];
+                            res.forEach(function(element) {
+                                var component = element.split(" ")[1];
+                                components.push(component);
+                            });
+                            jsonSortElem.components = components;*/
+                        }
+                        if (element.includes("Range")) {
+                            var range = element.split("</b> ")[1];
+                            jsonSortElem.range = range;
+                        }
+                        if (element.includes("Effect")) {
+                            var effect = element.split("</b> ")[1];
+                            jsonSortElem.effect = effect;
+                        }
+                        if (element.includes("Saving Throw")) {
+                            var saving_throw = element.split("</b> ")[1];
+                            jsonSortElem.saving_throw = saving_throw;
+                        }
+                        if (element.includes("Spell Resistance")) {
+                            var spell_resistance = element.split("</b> ")[1];
+                            var exp = /no/;
+                            if (exp.test(spell_resistance)) {
+                                spell_resistance = false;
+                            } else {
+                                spell_resistance = true;
+                            }
+                            jsonSortElem.spell_resistance = spell_resistance;
+                        }
                     });
+                });
 
-                    $('.SPDet').each(function(i, elem) {
-                        var data = $(this).html(); // doit renvoyer un truc du style (exemple): <b>School</b> evocation <b>Level</b> paladin 4, ranger 4
-                        // regarder le contenu de tous les <b>, si un des <b> contient un des éléments qu'on recherche, récupérer le texte qui est juste après
-                        var res = data.split("<b>");
-                        res.forEach(function(element) {
-                            if (element.includes("School")) {
-                                var school = element.split("</b> ")[1];
-                                jsonSortElem.school = school;
-                            }
-                            if (element.includes("Level")) {
-                                var res = element.split("</b> ")[1];
-                                res = element.split(",");
-                                var levels = [];
-                                res.forEach(function(element) {
-                                    var res2 = element.split(" ");
-                                    levels.push({"class":res2[1],"level":res2[2]});
-                                });
-                                jsonSortElem.levels = levels;
-                            }
-                            if (element.includes("Casting Time")) {
-                                var casting_time = element.split("</b> ")[1];
-                                jsonSortElem.casting_time = casting_time;
-                            }
-                            if (element.includes("Components")) {
-                                
-                                var res = element.split("</b> ")[1];
-                                
-                                var components = [];
-                                var regex = / *,? *(V|S|F|M|DF|F\/DF|M\/DF) *,? */g;
-                                while (match = regex.exec(res)) components.push(match[1]);
-                                jsonSortElem.components = components;
-                                
-                                /*var res = element.split("</b> ")[1];
-                                res = element.split(",");
-                                var components = [];
-                                res.forEach(function(element) {
-                                    var component = element.split(" ")[1];
-                                    components.push(component);
-                                });
-                                jsonSortElem.components = components;*/
-                            }
-                            if (element.includes("Range")) {
-                                var range = element.split("</b> ")[1];
-                                jsonSortElem.range = range;
-                            }
-                            if (element.includes("Effect")) {
-                                var effect = element.split("</b> ")[1];
-                                jsonSortElem.effect = effect;
-                            }
-                            if (element.includes("Saving Throw")) {
-                                var saving_throw = element.split("</b> ")[1];
-                                jsonSortElem.saving_throw = saving_throw;
-                            }
-                            if (element.includes("Spell Resistance")) {
-                                var spell_resistance = element.split("</b> ")[1];
-                                var exp = /no/;
-                                if (exp.test(spell_resistance)) {
-                                    spell_resistance = false;
-                                } else {
-                                    spell_resistance = true;
-                                }
-                                jsonSortElem.spell_resistance = spell_resistance;
-                            }
-                        });
-                    });
+                jsonSortElem.description = "";
 
-                    jsonSortElem.description = "";
-                    
-                    $('.SpellDiv > .SPDesc:nth-last-child(n+4)').filter(function(){
+                $('.SpellDiv > .SPDesc:nth-last-child(n+4)').filter(function(){
+                    var data = $(this);
+                    jsonSortElem.description = data.children().text();
+                });
+
+                if(jsonSortElem.description.length == 0) {
+                    $('.SpellDiv > .SPDesc:nth-last-child(n+2)').filter(function(){
                         var data = $(this);
                         jsonSortElem.description = data.children().text();
                     });
-                    
-                    if(jsonSortElem.description.length == 0) {
-                        $('.SpellDiv > .SPDesc:nth-last-child(n+2)').filter(function(){
-                            var data = $(this);
-                            jsonSortElem.description = data.children().text();
-                        });
-                    }
-
-                    sorts.push(jsonSortElem);
-                    callback();
                 }
-            });
 
-    }, function(err) {
-        
-        console.log("Fin de la récupération des sorts");
-        
-        /* Insertion en base de donnée MongoDB */
-//        console.log("Début de l'insertion des sorts avec MongoDB");
-//        
-//        insertSpells.insertMongoDB(sorts, function(result) {
-//            if(result) {
-//                console.log('Sorts inssérés dans la base de données MongoDB');
-//            } else {
-//                console.log('Echec lors de l\'insertion des sorts dans la base de données MongoDB');
-//            }
-//        });
-        
-        /* Insertion en base de donnée SQlite */
-        console.log("Début de l'insertion des sorts avec SQLite (peut prendre un peu de temps)");
-        
-        insertSpells.insertSQlite(sorts, function(err) {
-            if(err) console.log('Echec lors de l\'insertion des sorts dans la base de données SQlite');
-            else console.log('Sorts insérés dans la base de données SQlite');
+                sorts.push(jsonSortElem);
+                callback();
+            }
         });
-        
-        res.json(sorts);
-        
+
+}, function(err) {
+
+    console.log("Fin de la récupération des sorts");
+
+    /* Insertion en base de donnée MongoDB */
+    console.log("Début de l'insertion des sorts avec MongoDB");
+
+    insertSpells.insertMongoDB(sorts, function(result) {
+        if(result) console.log('Sorts inssérés dans la base de données MongoDB');
+        else console.log('Echec lors de l\'insertion des sorts dans la base de données MongoDB');
     });
 
-});
+    /* Insertion en base de donnée SQlite */
+//    console.log("Début de l'insertion des sorts avec SQLite (peut prendre un peu de temps)");
+//
+//    insertSpells.insertSQlite(sorts, function(err) {
+//        if(err) console.log('Echec lors de l\'insertion des sorts dans la BDD SQlite : ' + err);
+//        else console.log('Sorts insérés dans la base de données SQlite');
+//    });
 
-app.listen('8088')
-console.log('Lancement du serveur');
-exports = module.exports = app;
+});
--- exercice1/insertSpells.js
+++ exercice1/insertSpells.js
@@ -1,28 +1,14 @@
-/*-------------------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------------------*/
-/* Insertion de sorts dans la base MongoDB ou dans la base SQlite                      */
-/*-------------------------------------------------------------------------------------*/
-/*-------------------------------------------------------------------------------------*/
-
 var fs = require("fs");
 var async = require("async");
 
-/* Configuration MongoDB */
+/*-------------------------------------------------------------*/
+/*-------------------------------------------------------------*/
+/* Insertion de sorts dans la base MongoDB                     */
+/*-------------------------------------------------------------*/
+/*-------------------------------------------------------------*/
 
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/";
-
-/* Configuration SQlite */
-
-var sqlite3 = require('sqlite3').verbose();
-
-//Supprimer le fichier de la bdd s'il existe
-fs.unlink('./sqlitedb/sorts.db');
-
-var db = new sqlite3.Database('./sqlitedb/sorts.db', (err) => {
-  if (err) console.error(err.message);
-  console.log('Connected to the sorts database.');
-});
 
 var insertMongoDB = function(sortsJson, callback) {
     
@@ -30,8 +16,13 @@ var insertMongoDB = function(sortsJson, callback) {
         
         if (err) callback(false);
         var dbo = db.db("sorts");
-//        var sortsJson = fs.readFileSync("test.json");
-        var sorts = JSON.parse(sortsJson);  
+        
+        var sorts = sortsJson;
+        
+        //Truncate collection
+        dbo.collection("sorts").deleteMany(function(err, res) {
+            if (err) throw err;
+        });
         
         dbo.collection("sorts").insertMany(sorts, function(err, res) {
             
@@ -45,77 +36,91 @@ var insertMongoDB = function(sortsJson, callback) {
 
 }
 
+/*------------------------------------------------------------*/
+/*------------------------------------------------------------*/
+/* Insertion de sorts dans la base SQLite                     */
+/*------------------------------------------------------------*/
+/*------------------------------------------------------------*/
+
+var sqlite3 = require("sqlite3"), TransactionDatabase = require("sqlite3-transactions").TransactionDatabase;
+//Supprimer le fichier de la bdd s'il existe
+fs.unlink('./sqlitedb/sorts.db', (err) => {
+  if (err) console.log(err.message);
+});
+var db = new TransactionDatabase(new sqlite3.Database("./sqlitedb/sorts.db", sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE));
+
 var insertSQlite = function(sortsJson, callback) {
     
-    db.serialize(function () {
+    /* Création des tables nécessaires (on est obligé de séparer chaque table dans un fichier            */
+    /* car une seule action à la fois n'est possible avec run et il n'y a pas d'alternative avec sqlite3 */
+    db.run(fs.readFileSync('./sql_init/create_table_sort.sql', 'utf8'));
+    db.run(fs.readFileSync('./sql_init/create_table_component.sql', 'utf8'));
+    db.run(fs.readFileSync('./sql_init/create_table_level.sql', 'utf8'));
+    db.run(fs.readFileSync('./sql_init/create_table_sort_component.sql', 'utf8'));
+    db.run(fs.readFileSync('./sql_init/create_table_sort_level.sql', 'utf8'));
+    db.run(fs.readFileSync('./sql_init/insert_data_table_component.sql', 'utf8'));
+    db.run(fs.readFileSync('./sql_init/insert_data_table_level.sql', 'utf8'));
+    
+    // Commencer la transaction
+    db.beginTransaction(function(err, transaction) {
         
-        /* Création des tables nécessaires (on est obligé de séparer chaque table dans un fichier            */
-        /* car une seule action à la fois n'est possible avec run et il n'y a pas d'alternative avec sqlite3 */
-        
-        db.run(fs.readFileSync('./sql_init/create_table_sort.sql', 'utf8'));
-        db.run(fs.readFileSync('./sql_init/create_table_component.sql', 'utf8'));
-        db.run(fs.readFileSync('./sql_init/create_table_level.sql', 'utf8'));
-        db.run(fs.readFileSync('./sql_init/create_table_sort_component.sql', 'utf8'));
-        db.run(fs.readFileSync('./sql_init/create_table_sort_level.sql', 'utf8'));
-        db.run(fs.readFileSync('./sql_init/insert_data_table_component.sql', 'utf8'));
-        db.run(fs.readFileSync('./sql_init/insert_data_table_level.sql', 'utf8'));
-        
-        async.each(sortsJson, function(item, callback) {
-            
-            db.run('INSERT INTO sort(_id, name, school, casting_time, _range, effect, duration, saving_throw, spell_resistance, description) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                   [item._id, item.name, item.school, item.casting_time, item.range, item.effect, item.duration, item.saving_throw, item.spell_resistance, item.description],
-            function(err) {
-                
-                if (err) callback(err);
-                
-                else{
-                
-                    // Insertion dans SORT_LEVEL
-                    async.each(item.levels, function(itemLevel, callback) {
-
-                        db.get('SELECT _id FROM level WHERE name = ?', [itemLevel.class.toLowerCase()], (err, row) => {
-
-                            if(typeof row !== 'undefined'){
-                                db.run('INSERT INTO sort_level(_id_sort, _id_level, _value) VALUES(?, ?, ?)', [item._id, row._id, itemLevel.level], function(err) {
-                                    if (err) callback(err);
-                                    else callback();
-                                });
-                            } else callback();
-
-                        });
-
-                    }, function(err) {
-                        
-                        if (err) callback(err);
-                        else{
-                            // Insertion dans SORT_COMPONENTS
-                            async.each(item.components, function(itemComponent, callback) {
-
-                                db.get('SELECT _id FROM component WHERE name = ?', [itemComponent.toUpperCase()], (err, row) => {
-
-                                    db.run('INSERT INTO sort_component(_id_sort, _id_component) VALUES(?, ?)', [item._id, row._id], function(err) {
-                                        if (err) callback(err);
-                                        else callback();
-                                    });
-
-                                });
-
-                            }, function(err) {
-                                if(err)callback(err);
-                                else callback();
-                            }); 
-                        }
-                          
-                    });                   
-                }
-            });
-            
-        }, function(err) {
-            if(err)callback(err);
-            else callback();
-        });
-        
-    });
+         async.each(sortsJson, function(item, callback) {
+             
+             // Insertion dans SORT
+             transaction.run('INSERT INTO sort(_id, name, school, casting_time, _range, effect, duration, saving_throw, spell_resistance, description) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [item._id, item.name, item.school, item.casting_time, item.range, item.effect, item.duration, item.saving_throw, item.spell_resistance, item.description], function(err) {
+                 
+                 if (err) callback(err);
+                 else{
+                     
+                     // Insertion dans SORT_LEVEL
+                     async.each(item.levels, function(itemLevel, callback) {
+                         
+                         transaction.get('SELECT _id FROM level WHERE name = ?', [itemLevel.class.toLowerCase()], (err, row) => {
+                             
+                             if(typeof row !== 'undefined'){
+                                 
+                                 transaction.run('INSERT INTO sort_level(_id_sort, _id_level, _value) VALUES(?, ?, ?)', [item._id, row._id, itemLevel.level], function(err) {
+                                     if (err) callback(err);
+                                     else callback();
+                                 });
+                             } else callback();
+                         });
+                     
+                     }, function(err) {
+                         
+                         if (err) callback(err);
+                         else{
+                             
+                             // Insertion dans SORT_COMPONENTS
+                             async.each(item.components, function(itemComponent, callback) {
+                                 
+                                 transaction.get('SELECT _id FROM component WHERE name = ?', [itemComponent.toUpperCase()], (err, row) => {
+                                     
+                                     transaction.run('INSERT INTO sort_component(_id_sort, _id_component) VALUES(?, ?)', [item._id, row._id], function(err) {
+                                         if (err) callback(err);
+                                         else callback();
+                                     });
+                                 });
+                                 
+                             }, function(err) {
+                                 if(err)callback(err);
+                                 else callback();
+                             });
+                         }
+                     });
+                 }
+             });
+         
+         }, function(err) {
+             if(err)callback(err);
+             else{
+                 transaction.commit(function(err) {
+                     if (err) callback(err);
+                     else callback();
+                 });
+             }
+         });
+    });  
 }
 
 
