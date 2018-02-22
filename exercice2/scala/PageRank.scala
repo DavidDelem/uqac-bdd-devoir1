2ac385ebb666f7017dcb37f28ed1674505e5c1b5
Paul <paul.du56@gmail.com>
2018-02-21 19:47:49 -0500

    début en scala (insertion bdd)

--- exercice2/scala/PageRank.scala
+++ exercice2/scala/PageRank.scala
@@ -1,3 +1,53 @@
+import org.mongodb.scala._
+import Helpers._
+
 object PageRank extends App {
-    println("Hello World from Scala!")
+
+  val mongoClient: MongoClient = MongoClient()
+  val database: MongoDatabase = mongoClient.getDatabase("pagerank")
+  val collection: MongoCollection[Document] = database.getCollection("data")
+
+
+  val A: Document = Document(
+    "_id" -> "A",
+    "value" -> Document(
+      "url" -> "A",
+      "pagerank" -> 1,
+      "outlink_list" -> List("B","C")
+    )
+  )
+
+  val B: Document = Document(
+    "_id" -> "B",
+    "value" -> Document(
+      "url" -> "B",
+      "pagerank" -> 1,
+      "outlink_list" -> List("C")
+    )
+  )
+
+  val C: Document = Document(
+    "_id" -> "C",
+    "value" -> Document(
+      "url" -> "C",
+      "pagerank" -> 1,
+      "outlink_list" -> List("A")
+    )
+  )
+
+  val D: Document = Document(
+    "_id" -> "D",
+    "value" -> Document(
+      "url" -> "D",
+      "pagerank" -> 1,
+      "outlink_list" -> List("C")
+    )
+  )
+
+  collection.insertOne(A).results();
+  collection.insertOne(B).results();
+  collection.insertOne(C).results();
+  collection.insertOne(D).results();
+
+
 }
